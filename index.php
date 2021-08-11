d74b0b1df264aa3fd91d5c26a817e37288f36695
Arda Mazreku <ardamazreku99@gmail.com>
2021-08-11 18:30:46 +0200

    Update

--- index.php
+++ index.php
@@ -46,8 +46,9 @@
         <div class="nk-image-slider-item">
             <img src="src/assets/images/slide-1.jpg" alt="" class="nk-image-slider-img" data-thumb="src/assets/images/slide-1.jpg">
             <div class="nk-image-slider-content">
-                <h3 class="h4">As we Passed, I remarked</h3>
-                <p class="text-white">As we passed, I remarked a beautiful church-spire rising above some old elms in the park; and before them, in the midst of a lawn, chimneys covered with ivy, and the windows shining in the sun.</p>
+                <h3 class="h4"> Black Ops Cold War </h3>
+                <p class="text-white">As the Zombie Outbreak grows, only the most fearless Operators will answer the call.
+                <br> A mountain of new Zombies content is coming to #BlackOpsColdWar this week. Get ready to climb! </p>
                 <a href="#" class="nk-btn nk-btn-rounded nk-btn-color-white nk-btn-hover-color-main-1">Read More</a>
             </div>
         </div>
@@ -55,8 +56,9 @@
         <div class="nk-image-slider-item">
             <img src="src/assets/images/slide-2.jpg" alt="" class="nk-image-slider-img" data-thumb="src/assets/images/slide-2.jpg">
             <div class="nk-image-slider-content">
-                    <h3 class="h4">He made his passenger captain of one</h3>
-                    <p class="text-white">Now the races of these two have been for some ages utterly extinct, and besides to discourse any further of them would not be at all to my purpose. But the concern I have most at heart is for our Corporation of Poets, from whom I am preparing a petition to your Highness,  to be subscribed with the names of onesrc.</p>
+                    <h3 class="h4"> Assasin's Creed VALHALLA </h3>
+                    <p class="text-white"> The wait is over! 0 days left until kings and rats and castles alike. <br>
+                    The Siege of Paris launched on August 12th! </p>
                     <a href="#" class="nk-btn nk-btn-rounded nk-btn-color-white nk-btn-hover-color-main-1">Read More</a>
             </div>
         </div>
@@ -65,21 +67,21 @@
             <img src="src/assets/images/slide-3.jpg" alt="" class="nk-image-slider-img" data-thumb="src/assets/images/slide-3.jpg">
         </div>
         <div class="nk-image-slider-item">
-            <img src="src/assets/images/slide-4.jpg" alt="" class="nk-image-slider-img" data-thumb="src/assets/images/slide-4-thumb.jpg">
+            <img src="src/assets/images/slide-4.jpg" alt="" class="nk-image-slider-img" data-thumb="src/assets/images/slide-4.jpg">
             <div class="nk-image-slider-content">
-                    <h3 class="h4">At length one of them called out in a clear</h3>
-                    <p class="text-white">TJust then her head struck against the roof of the hall: in fact she was now more than nine feet highsrc.</p>
+                    <h3 class="h4"> Forza Horizon 5's map is here and the volcano is basically Mount Doom </h3>
+                    <p class="text-white">La Gran Caldera dominates the northwest quadrant of the map,
+                    and will probably loom large no matter where you are. It's going to be the jungle gym of the map,
+                    where freeroam players zip around to catch major air off of its rim & bumpy sides.</p>
                     <a href="#" class="nk-btn nk-btn-rounded nk-btn-color-white nk-btn-hover-color-main-1">Read More</a>
             </div>
         </div>
         
         <div class="nk-image-slider-item">
-            <img src="src/assets/images/slide-5.jpg" alt="" class="nk-image-slider-img" data-thumb="src/assets/images/slide-5-thumb.jpg">
+            <img src="src/assets/images/slide-5.jpg" alt="" class="nk-image-slider-img" data-thumb="src/assets/images/slide-5.jpg">
             <div class="nk-image-slider-content">
-                    <h3 class="h4">For good, too though, in consequence</h3>
-                    <p class="text-white">She gave my mother such a turn, that I have always been convinced I am indebted to Miss Betsey for having been born on a Friday. The word was appropriate to the moment.</p>
-                    <p class="text-white">My mother was so much worse that Peggotty, coming in with the teaboard and candles, and seeing at a glance how ill she was, - as Miss Betsey might have done sooner if there had been light enough, - conveyed her upstairs to her own room with all speed; and immediately dispatched Ham Peggotty, her nephew, who had been for some days past secreted in the housesrc.</p>
-                    <a href="#" class="nk-btn nk-btn-rounded nk-btn-color-white nk-btn-hover-color-main-1">Read More</a>
+                    <h3 class="h4"> Four Of The Best PC Games Ever Are Free Right Now </h3>
+                    <p class="text-white">Ultima Underworlds 1 & 2, Syndicate, and Syndicate Wars are back—and until September 3, they’re free! </p>
             </div>
         </div>
     </div>
@@ -592,9 +594,6 @@
         </div>
     </div>
 </div>
-
-
-
                         <!-- END: MMO Tab -->
                         <div class="nk-gap"></div>
                     </div>
--- src/components/modals.php
+++ src/components/modals.php
@@ -47,7 +47,49 @@
                         <div class="col-md-12">
                             <a href="#" class="nk-btn nk-btn-rounded nk-btn-color-white nk-btn-block">Sign In</a>
                             <br>
-                            <b><a href="signup.php">Not a member? Sign up! </a></b>
+                            <b><a href ="#" data-toggle="modal" data-target="#modalSignup">Not a member? Sign up! </a></b>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- END: Login Modal -->
+
+<!-- START: Sign Up Modal -->
+<div class="nk-modal modal fade" id="modalSignup" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal-dialog modal-sm" role="document">
+        <div class="modal-content">
+            <div class="modal-body">
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span class="ion-android-close"></span>
+                </button>
+
+                <h4 class="mb-0"><span class="text-main-1">Sign</span> Up </h4>
+
+                <div class="nk-gap-1"></div>
+                <form action="#" class="nk-form text-white">
+                    <div class="row vertical-gap">
+                        <div class="col-md-12">
+                            Name:
+                            <div class="nk-gap"></div>
+                            <input type="text" value="" name="name" class=" form-control" placeholder="Name">
+                            <br> Surname:
+                            <div class="nk-gap"></div>
+                            <input type="text" value="" name="surname" class=" form-control" placeholder="Surname">
+                            <br> Email:
+                            <div class="nk-gap"></div>
+                            <input type="email" value="" name="email" class=" form-control" placeholder="Email">
+                            <br> Password:
+                            <div class="nk-gap"></div>
+                            <input type="password" value="" name="password" class="required form-control" placeholder="Password">
+                        </div>
+                    </div>
+                    <div class="nk-gap-1"></div>
+                    <div class="row vertical-gap">
+                        <div class="col-md-12">
+                            <a href="#" class="nk-btn nk-btn-rounded nk-btn-color-white nk-btn-block"> Register </a>
                         </div>
                     </div>
                 </form>
