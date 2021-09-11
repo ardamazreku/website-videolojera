a94a40dad99010828bc125df054de9b196cdc275
Arda Mazreku <ardamazreku99@gmail.com>
2021-09-11 19:51:25 +0200

    Updated store.php

--- store.php
+++ store.php
@@ -1,78 +1,54 @@
 <?php
 session_start();
+if(isset($_SESSION['email'])) {
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-
-    <title>GoodGames | Store</title>
-
-    <meta name="description" content="GoodGames - Bootstrap template for communities and games store">
-    <meta name="keywords" content="game, gaming, template, HTML template, responsive, Bootstrap, premium">
-    <meta name="author" content="_nK">
-
-    <link rel="icon" type="image/png" href="src/assets/images/favicon.png">
-
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- START: Styles -->
-
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700%7cOpen+Sans:400,700" rel="stylesheet" type="text/css">
-
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="src/assets/vendor/bootstrap/dist/css/bootstrap.min.css">
-
-    <!-- FontAwesome -->
-    <script defer src="src/assets/vendor/fontawesome-free/js/all.js"></script>
-    <script defer src="src/assets/vendor/fontawesome-free/js/v4-shims.js"></script>
-
-    <!-- IonIcons -->
-    <link rel="stylesheet" href="src/assets/vendor/ionicons/css/ionicons.min.css">
-
-    <!-- Flickity -->
-    <link rel="stylesheet" href="src/assets/vendor/flickity/dist/flickity.min.css">
-
-    <!-- Photoswipe -->
-    <link rel="stylesheet" type="text/css" href="src/assets/vendor/photoswipe/dist/photoswipe.css">
-    <link rel="stylesheet" type="text/css" href="src/assets/vendor/photoswipe/dist/default-skin/default-skin.css">
-
-    <!-- Seiyria Bootstrap Slider -->
-    <link rel="stylesheet" href="src/assets/vendor/bootstrap-slider/dist/css/bootstrap-slider.min.css">
-
-    <!-- Summernote -->
-    <link rel="stylesheet" type="text/css" href="src/assets/vendor/summernote/dist/summernote-bs4.css">
-
-    <!-- GoodGames -->
-    <link rel="stylesheet" href="src/assets/css/app.css">
-
-    <!-- Custom Styles -->
-    <link rel="stylesheet" href="src/assets/css/custom.css">
-    
-    <!-- END: Styles -->
-
-    <!-- jQuery -->
-    <script src="src/assets/vendor/jquery/dist/jquery.min.js"></script>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<title> SteCord | Game Store </title>
+<meta name="description" content="SteCord - Gaming community and store">
+<meta name="keywords" content="game, gaming, Bootstrap, premium">
+<meta name="author" content="Arda Mazreku">
+<link rel="icon" type="image/png" href="public/favicon.png">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- Google Fonts -->
+<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700%7cOpen+Sans:400,700" rel="stylesheet" type="text/css">
+<!-- Bootstrap -->
+<link rel="stylesheet" href="src/assets/vendor/bootstrap/dist/css/bootstrap.min.css">
+<link rel="stylesheet" href="src/assets/vendor/bootstrap/dist/css/bootstrap-grid.min.css">
+<!-- FontAwesome -->
+<script defer src="src/assets/vendor/fontawesome-free/js/all.js"></script>
+<script defer src="src/assets/vendor/fontawesome-free/js/v4-shims.js"></script>
+<!-- IonIcons -->
+<link rel="stylesheet" href="src/assets/vendor/ionicons/css/ionicons.min.css">
+<!-- Flickity -->
+<link rel="stylesheet" href="src/assets/vendor/flickity/dist/flickity.min.css">
+<!-- Photoswipe -->
+<link rel="stylesheet" type="text/css" href="src/assets/vendor/photoswipe/dist/photoswipe.css">
+<link rel="stylesheet" type="text/css" href="src/assets/vendor/photoswipe/dist/default-skin/default-skin.css">
+<!-- Seiyria Bootstrap Slider -->
+<link rel="stylesheet" href="src/assets/vendor/bootstrap-slider/dist/css/bootstrap-slider.min.css">
+<!-- Summernote -->
+<link rel="stylesheet" type="text/css" href="src/assets/vendor/summernote/dist/summernote-bs4.css">
+<!-- Style Css -->
+<link rel="stylesheet" href="src/assets/css/app.css">
+<!-- jQuery -->
+<script src="src/assets/vendor/jquery/dist/jquery.min.js"></script>
 </head>
 <body>
 <?php include "src/components/navbar.php";?>
 <div class="nk-main">
         
-            <!-- START: Breadcrumbs -->
+<!-- START: Breadcrumbs -->
 <div class="nk-gap-1"></div>
 <div class="container">
     <ul class="nk-breadcrumbs">
-        
-        
         <li><a href="index.php">Home</a></li>
-        
         
         <li><span class="fa fa-angle-right"></span></li>
         
         <li><span>Store</span></li>
-        
     </ul>
 </div>
 <div class="nk-gap-1"></div>
@@ -87,8 +63,7 @@ session_start();
                     <img src="src/assets/images/icon-mouse.png" alt="">
                 </div>
                 <div class="nk-feature-cont">
-                    <h3 class="nk-feature-title"><a href="#">PC</a></h3>
-                    <h3 class="nk-feature-title text-main-1"><a href="#">View Games</a></h3>
+                    <h3 class="nk-feature-title">PC</h3>
                 </div>
             </div>
         </div>
@@ -98,88 +73,22 @@ session_start();
                     <img src="src/assets/images/icon-gamepad.png" alt="">
                 </div>
                 <div class="nk-feature-cont">
-                    <h3 class="nk-feature-title"><a href="#">PS4</a></h3>
-                    <h3 class="nk-feature-title text-main-1"><a href="#">View Games</a></h3>
+                    <h3 class="nk-feature-title"> PS & XBOX </h3>
                 </div>
             </div>
         </div>
         <div class="col-lg-4">
             <div class="nk-feature-1">
                 <div class="nk-feature-icon">
-                    <img src="src/assets/images/icon-gamepad-2.png" alt="">
+                    <img src="src/assets/images/icon-nintendo.png" width="60%" alt="">
                 </div>
                 <div class="nk-feature-cont">
-                    <h3 class="nk-feature-title"><a href="#">Xbox</a></h3>
-                    <h3 class="nk-feature-title text-main-1"><a href="#">View Games</a></h3>
+                    <h3 class="nk-feature-title">Nintendo</h3>
                 </div>
             </div>
         </div>
     </div>
     <!-- END: Categories -->
-
-    <!-- START: Some Products -->
-    <div class="nk-gap-2"></div>
-    <div class="row vertical-gap">
-        <div class="col-md-6 col-lg-4">
-            <div class="nk-gallery-item-box">
-                <a href="store-product.php" class="nk-gallery-item">
-                    <span class="nk-gallery-item-overlay"><span><span class="nk-icon-circles"></span></span></span>
-                    <img src="src/assets/images/product-4-lg.jpg" alt="She was bouncing">
-                </a>
-                <div class="nk-gallery-item-label">
-                    <h4 class="mb-0">She was bouncing</h4>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-6 col-lg-4 order-lg-3">
-            <div class="nk-gallery-item-box">
-                <a href="store-product.php" class="nk-gallery-item">
-                    <span class="nk-gallery-item-overlay"><span><span class="nk-icon-circles"></span></span></span>
-                    <img src="src/assets/images/product-2-lg.jpg" alt="However, I have reason">
-                </a>
-                <div class="nk-gallery-item-label">
-                    <h4 class="mb-0">However, I have reason</h4>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-12 col-lg-4">
-            <div class="row vertical-gap">
-                <div class="col-md-6">
-                    <div class="nk-gallery-item-box">
-                        <a href="store-product.php" class="nk-gallery-item">
-                            <span class="nk-gallery-item-overlay"><span><span class="nk-icon-circles"></span></span></span>
-                            <img src="src/assets/images/product-3-lg.jpg" alt="It was some time before">
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="nk-gallery-item-box">
-                        <a href="store-product.php" class="nk-gallery-item">
-                            <span class="nk-gallery-item-overlay"><span><span class="nk-icon-circles"></span></span></span>
-                            <img src="src/assets/images/product-1-lg.jpg" alt="So saying he unbuckled">
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="nk-gallery-item-box">
-                        <a href="store-product.php" class="nk-gallery-item">
-                            <span class="nk-gallery-item-overlay"><span><span class="nk-icon-circles"></span></span></span>
-                            <img src="src/assets/images/product-5-lg.jpg" alt="In all revolutions of">
-                        </a>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="nk-gallery-item-box">
-                        <a href="store-product.php" class="nk-gallery-item">
-                            <span class="nk-gallery-item-overlay"><span><span class="nk-icon-circles"></span></span></span>
-                            <img src="src/assets/images/product-6-lg.jpg" alt="Just then her head ">
-                        </a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- END: Some Products -->
 
     <!-- START: Top 10 Best Sellers -->
     <div class="nk-gap-3"></div>
@@ -187,7 +96,6 @@ session_start();
     <div class="nk-gap"></div>
     <div class="nk-carousel nk-carousel-x4" data-autoplay="5000" data-dots="false" data-cell-align="left" data-arrows="true">
         <div class="nk-carousel-inner">
-            
             
             <div><div class="pl-5 pr-5">
                 <div class="nk-product-cat-3">
@@ -538,132 +446,7 @@ session_start();
 </div>
 
 <div class="nk-gap-2"></div>
-
-
-        
-            <!-- START: Footer -->
-<footer class="nk-footer">
-
-    <div class="container">
-        <div class="nk-gap-3"></div>
-        <div class="row vertical-gap">
-            <div class="col-md-6">
-                <div class="nk-widget">
-                    <h4 class="nk-widget-title"><span class="text-main-1">Contact</span> With Us</h4>
-                    <div class="nk-widget-content">
-                        <form action="ajax-contact-form.php" class="nk-form nk-form-ajax">
-                            <div class="row vertical-gap sm-gap">
-                                <div class="col-md-6">
-                                    <input type="email" class="form-control required" name="email" placeholder="Email *">
-                                </div>
-                                <div class="col-md-6">
-                                    <input type="text" class="form-control required" name="name" placeholder="Name *">
-                                </div>
-                            </div>
-                            <div class="nk-gap"></div>
-                            <textarea class="form-control required" name="message" rows="5" placeholder="Message *"></textarea>
-                            <div class="nk-gap-1"></div>
-                            <button class="nk-btn nk-btn-rounded nk-btn-color-white">
-                                <span>Send</span>
-                                <span class="icon"><i class="ion-paper-airplane"></i></span>
-                            </button>
-                            <div class="nk-form-response-success"></div>
-                            <div class="nk-form-response-error"></div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="nk-widget">
-                    <h4 class="nk-widget-title"><span class="text-main-1">Latest</span> Posts</h4>
-                    <div class="nk-widget-content">
-                        <div class="row vertical-gap sm-gap">
-                            
-                            <div class="col-lg-6">
-                                <div class="nk-widget-post-2">
-                                    <a href="blog-article.php" class="nk-post-image">
-                                        <img src="src/assets/images/post-1-sm.jpg" alt="">
-                                    </a>
-                                    <div class="nk-post-title"><a href="blog-article.php">Smell magic in the air. Or maybe barbecue</a></div>
-                                    <div class="nk-post-date">
-                                        <span class="fa fa-calendar"></span> Sep 18, 2018
-                                        <span class="fa fa-comments"></span> <a href="#">4</a>
-                                    </div>
-                                </div>
-                            </div>
-                            
-                            <div class="col-lg-6">
-                                <div class="nk-widget-post-2">
-                                    <a href="blog-article.php" class="nk-post-image">
-                                        <img src="src/assets/images/post-2-sm.jpg" alt="">
-                                    </a>
-                                    <div class="nk-post-title"><a href="blog-article.php">Grab your sword and fight the Horde</a></div>
-                                    <div class="nk-post-date">
-                                        <span class="fa fa-calendar"></span> Sep 5, 2018
-                                        <span class="fa fa-comments"></span> <a href="#">7</a>
-                                    </div>
-                                </div>
-                            </div>
-                            
-                        </div>
-                    </div>
-                </div>
-                <div class="nk-widget">
-                    <h4 class="nk-widget-title"><span class="text-main-1">In</span> Twitter</h4>
-                    <div class="nk-widget-content">
-                        <div class="nk-twitter-list" data-twitter-count="1"></div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="nk-gap-3"></div>
-    </div>
-
-    <div class="nk-copyright">
-        <div class="container">
-            <div class="nk-copyright-left">
-                <a target="_blank" href="https://www.templateshub.net">Templates Hub</a>
-            </div>
-            <div class="nk-copyright-right">
-                <ul class="nk-social-links-2">
-                    <li><a class="nk-social-rss" href="#"><span class="fa fa-rss"></span></a></li>
-                    <li><a class="nk-social-twitch" href="#"><span class="fab fa-twitch"></span></a></li>
-                    <li><a class="nk-social-steam" href="#"><span class="fab fa-steam"></span></a></li>
-                    <li><a class="nk-social-facebook" href="#"><span class="fab fa-facebook"></span></a></li>
-                    <li><a class="nk-social-google-plus" href="#"><span class="fab fa-google-plus"></span></a></li>
-                    <li><a class="nk-social-twitter" href="#" target="_blank"><span class="fab fa-twitter"></span></a></li>
-                    <li><a class="nk-social-pinterest" href="#"><span class="fab fa-pinterest-p"></span></a></li>
-
-                    <!-- Additional Social Buttons
-                        <li><a class="nk-social-behance" href="#"><span class="fab fa-behance"></span></a></li>
-                        <li><a class="nk-social-bitbucket" href="#"><span class="fab fa-bitbucket"></span></a></li>
-                        <li><a class="nk-social-dropbox" href="#"><span class="fab fa-dropbox"></span></a></li>
-                        <li><a class="nk-social-dribbble" href="#"><span class="fab fa-dribbble"></span></a></li>
-                        <li><a class="nk-social-deviantart" href="#"><span class="fab fa-deviantart"></span></a></li>
-                        <li><a class="nk-social-flickr" href="#"><span class="fab fa-flickr"></span></a></li>
-                        <li><a class="nk-social-foursquare" href="#"><span class="fab fa-foursquare"></span></a></li>
-                        <li><a class="nk-social-github" href="#"><span class="fab fa-github"></span></a></li>
-                        <li><a class="nk-social-instagram" href="#"><span class="fab fa-instagram"></span></a></li>
-                        <li><a class="nk-social-linkedin" href="#"><span class="fab fa-linkedin"></span></a></li>
-                        <li><a class="nk-social-medium" href="#"><span class="fab fa-medium"></span></a></li>
-                        <li><a class="nk-social-odnoklassniki" href="#"><span class="fab fa-odnoklassniki"></span></a></li>
-                        <li><a class="nk-social-paypal" href="#"><span class="fab fa-paypal"></span></a></li>
-                        <li><a class="nk-social-reddit" href="#"><span class="fab fa-reddit"></span></a></li>
-                        <li><a class="nk-social-skype" href="#"><span class="fab fa-skype"></span></a></li>
-                        <li><a class="nk-social-soundcloud" href="#"><span class="fab fa-soundcloud"></span></a></li>
-                        <li><a class="nk-social-slack" href="#"><span class="fab fa-slack"></span></a></li>
-                        <li><a class="nk-social-tumblr" href="#"><span class="fab fa-tumblr"></span></a></li>
-                        <li><a class="nk-social-vimeo" href="#"><span class="fab fa-vimeo"></span></a></li>
-                        <li><a class="nk-social-vk" href="#"><span class="fab fa-vk"></span></a></li>
-                        <li><a class="nk-social-wordpress" href="#"><span class="fab fa-wordpress"></span></a></li>
-                        <li><a class="nk-social-youtube" href="#"><span class="fab fa-youtube"></span></a></li>
-                    -->
-                </ul>
-            </div>
-        </div>
-    </div>
-</footer>
-<!-- END: Footer -->
+<?php include"src/components/footer.php"; ?>
 </div>
 
 <!-- START: Page Background -->
@@ -673,3 +456,10 @@ session_start();
 <?php include "scripts.php";?>
 </body>
 </html>
+<?php
+}
+else {
+    echo'<script> alert("You have to log in order to access the store!"); 
+         location.replace("login.php"); </script>';
+}
+?>
