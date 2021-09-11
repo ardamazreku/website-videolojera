af39739cc35663b512c002ab842c369a40a607e1
Arda Mazreku <ardamazreku99@gmail.com>
2021-09-11 14:14:00 +0200

    Updated database

--- database/videolojera.sql
+++ database/videolojera.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.9.2
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3307
--- Generation Time: Sep 03, 2021 at 04:39 PM
--- Server version: 10.4.13-MariaDB
--- PHP Version: 7.3.21
+-- Host: 127.0.0.1:3306
+-- Generation Time: Sep 11, 2021 at 12:12 PM
+-- Server version: 10.4.10-MariaDB
+-- PHP Version: 7.3.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
