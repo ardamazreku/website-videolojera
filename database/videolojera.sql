1fd4b1300e747ee412dec3addf8d8a08c09b2cf2
Arda Mazreku <ardamazreku99@gmail.com>
2021-12-04 23:01:24 +0100

    Updated database data

--- database/videolojera.sql
+++ database/videolojera.sql
@@ -3,8 +3,8 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3307
--- Generation Time: Sep 26, 2021 at 07:25 PM
--- Server version: 10.4.13-MariaDB
+-- Generation Time: Dec 04, 2021 at 09:31 PM
+-- Server version: 10.5.4-MariaDB
 -- PHP Version: 7.3.21
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
@@ -232,9 +232,9 @@ CREATE TABLE IF NOT EXISTS `perdoruesi` (
 --
 
 INSERT INTO `perdoruesi` (`id`, `roli_id`, `email`, `emri`, `mbiemri`, `nickname`, `password`, `foto`, `data_regj`) VALUES
-(1, 1, 'ardamazreku99@gmail.com', 'Arda', 'Mazreku', '', 'Arda99!', 'src/assets/images/avatar-3.jpg', '2021-07-01'),
-(2, 1, 'dafinaveseli@gmail.com', 'Dafina ', 'Veseli', '', 'Dafina99!', 'src/assets/images/avatar-2.jpg', '2021-08-01'),
-(3, 1, 'festamazreku@gmail.com', 'Festa', 'Mazreku', '', 'Festa99!', '', '2021-08-01'),
+(1, 1, 'ardamazreku99@gmail.com', 'Arda', 'Mazreku', 'Arda Mazreku', 'Arda99!', 'src/assets/images/avatar-3.jpg', '2021-07-01'),
+(2, 1, 'dafinaveseli@gmail.com', 'Dafina ', 'Veseli', 'Dafina Veseli', 'Dafina99!', 'src/assets/images/avatar-2.jpg', '2021-08-01'),
+(3, 1, 'festamazreku@gmail.com', 'Festa', 'Mazreku', 'Festa Mazreku', 'Festa99!', '', '2021-08-01'),
 (4, 2, 'user123@gmail.com', 'Skyler', 'Johnson', 'SkyLerR12', 'skyLeR24!', 'src/assets/images/gallery-group-4', '2021-08-15'),
 (5, 2, 'adriangaming@hotmail.com', 'Adrian', 'Gaming', 'adDriAnN', 'adDriAnN123', 'src/assets/images/gallery-9-thumb', '2021-08-24'),
 (6, 2, 'bernardnekk@gmail.com', 'Bernard', 'Nekken', 'BernyBoi99', 'BernardNekk1999', 'src/assets/images/gallery-group-5.jpg', '2021-08-05'),
